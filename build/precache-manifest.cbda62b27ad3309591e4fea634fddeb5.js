3f034cb33f10275266a15a0da24d2c1b44b23d6e
Samir Murati <samirmurati@samirs-macbook-pro.home>
2022-01-24 12:51:14 +0100

    fixes a bug

---
+++ build/precache-manifest.cbda62b27ad3309591e4fea634fddeb5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9530651553b18a31d096cd56e2457ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bbcf043c1e580d7ce2b",
+    "url": "/static/js/2.e57ba182.chunk.js"
+  },
+  {
+    "revision": "6de3897fa82eee628887",
+    "url": "/static/js/main.360f0c4d.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
   return (
